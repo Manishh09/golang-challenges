b176b4129f55affc7f4241b2b541f28531a367d0
Manish Boge (SX/EDC2) <manish.boge@in.bosch.com>
2024-10-04 17:44:37 +0530

    add: challenge -1 : reversing a number

---
+++ challenge-1/reverse-number.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+Challenge-1:
+
+Write a function to print reverse of the number provided.
+
+Constraints:
+
+Input should be accepted through command-line
+
+N being the number of digits in a number, and  should be between 3 to 6
+
+E.g N = 345  OR 3456 or 34567 or 345678
+
+Tests:
+
+	Input							Output
+__________________________________________________
+	12354							19298
+
+	45321							89291
+
+*/
+
+func main() {
+
+}
